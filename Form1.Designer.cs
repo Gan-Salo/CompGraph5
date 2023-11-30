684a494f4aca499c152650fea20761760a030415
Gan-Salo <90299377+Gan-Salo@users.noreply.github.com>
2023-11-30 23:06:33 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 477);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(173, 477);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 27);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(26, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(524, 417);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(567, 23);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(268, 300);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(575, 380);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(575, 412);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(661, 389);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1053, 516);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap originalImage;
+        Dictionary<int, int> histogram = new Dictionary<int, int>();
+        private int imageWidth;
+        private int imageHeight;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.bmp;*.jpg;*.jpeg;*.png;*.gif;*.tiff";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                originalImage = new Bitmap(openFileDialog.FileName);
+                imageWidth = originalImage.Width;
+                imageHeight = originalImage.Height;
+                label1.Text = originalImage.Width.ToString();
+                label2.Text = originalImage.Height.ToString();
+                DisplayImage();
+            }
+        }
+        private void DisplayImage()
+        {
+            if (originalImage != null)
+            {
+                pictureBox1.Image = originalImage;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+                DrawHistogram();
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            histogram.Clear();
+            pictureBox2.Image = null;
+            // Перерисовка pictureBox2
+            pictureBox2.Invalidate();
+        }
+
+        private void DrawHistogram()
+        {
+            // Build histogram
+            //Dictionary<int, int> histogram = new Dictionary<int, int>(); // Assuming 8-bit grayscale image
+
+            for (int y = 0; y < originalImage.Height; y++)
+            {
+                for (int x = 0; x < originalImage.Width; x++)
+                {
+                    Color pixelColor = originalImage.GetPixel(x, y);
+                    int brightness = (int)(0.299 * pixelColor.R + 0.5876 * pixelColor.G + 0.114 * pixelColor.B);
+
+                    if (histogram.TryGetValue(brightness, out int count))
+                        histogram[brightness] = count + 1;
+                    else
+                        histogram[brightness] = 1;
+                }
+            }
+            // Sort keys in ascending order
+            var sortedKeys = histogram.Keys.OrderBy(k => k).ToList();
+
+            Console.WriteLine("Brightness\tPixel Count");
+            foreach (var entry in histogram)
+            {
+                Console.WriteLine($"{entry.Key}\t\t{entry.Value}");
+            }
+
+            // Get the maximum histogram value
+            int maxHistogramValue = histogram.Values.Max();
+            label3.Text = histogram.Values.Max().ToString();
+            // Подписи к осям
+            int maxBrightnessLabelInterval = 50; // Интервал для подписей к осям
+            int labelY = pictureBox2.Height + 30; // Исходное значение Y для подписей
+
+            int maxLabelValue = maxHistogramValue;
+            // Draw histogram
+            pictureBox2.Paint += (s, ev) =>
+            {
+                Pen pen = new Pen(Color.Black);
+                int barWidth = 2;
+                int offsetX = 5; // Расстояние от краев по оси X
+                foreach (var entry in histogram)
+                {
+                    int i = entry.Key;
+                    float normalizedValue = entry.Value / (float)maxHistogramValue;
+                    int barHeight = (int)(normalizedValue * pictureBox2.Height);
+                    
+                    float normalizedX = (float)i / (float)sortedKeys.Max();
+                    int x = (int)(normalizedX * (pictureBox2.Width - 2 * offsetX)) + offsetX;
+                    int y = pictureBox2.Height - barHeight;
+
+                    ev.Graphics.DrawLine(pen, x, pictureBox2.Height, x, y);
+                    //// Подписи к осям
+                    //if (i % 50 == 0)
+                    //{
+                    //    ev.Graphics.DrawString(entry.ToString(), Font, Brushes.Black, 0, y - 10);
+                    //}
+
+                    //// Распределение подписей к осям
+                    //if (i % 50 == 0)
+                    //{
+                    //    int textX = x + barWidth / 2 - 10; // Добавьте сдвиг для лучшей читаемости
+                    //    int textY = pictureBox2.Height + 5;
+                    //    ev.Graphics.DrawString(i.ToString(), Font, Brushes.Black, textX, textY);
+                    //}
+
+                    //if (i % 20 == 0)
+                    //{
+                    //    int textX = 0;
+                    //    int textY = y - 10;
+                    //    ev.Graphics.DrawString(entry.Value.ToString(), Font, Brushes.Black, textX, textY);
+                    //}
+                }
+
+                // Подписи к осям
+                
+                // Отрисовка вертикальных подписей
+                for (int i = 0; i <= pictureBox2.Height; i += pictureBox2.Height / 10)
+                {
+                    ev.Graphics.DrawString(((maxLabelValue * (pictureBox2.Height - i)) / pictureBox2.Height).ToString(), Font, Brushes.Black, 0, i);
+                }
+
+                // Подписи осей              
+                //ev.Graphics.DrawString("Яркость", Font, Brushes.Black, pictureBox2.Width / 2, labelY);
+                //ev.Graphics.DrawString("Число пикселей", Font, Brushes.Black, -20, pictureBox2.Height / 2, new StringFormat { FormatFlags = StringFormatFlags.DirectionVertical });
+            };
+
+            pictureBox2.Invalidate(); // Trigger the Paint event
+        }
+    }
+}
